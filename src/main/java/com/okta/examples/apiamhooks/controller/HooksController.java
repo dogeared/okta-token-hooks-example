9a92fc15446cac596449a21d8b0b1d85cf62a81d
Micah Silverman <micah@afitnerd.com>
2019-12-18 15:07:38 +0200

    removed unnecessary create hook code

--- src/main/java/com/okta/examples/apiamhooks/controller/HooksController.java
+++ src/main/java/com/okta/examples/apiamhooks/controller/HooksController.java
@@ -1,33 +1,27 @@
 package com.okta.examples.apiamhooks.controller;
 
 import com.okta.examples.apiamhooks.model.Beer;
-import com.okta.examples.apiamhooks.model.hooks.CreateHookRequest;
-import com.okta.examples.apiamhooks.model.hooks.IDTokenPatchResponse;
 import com.okta.examples.apiamhooks.model.Person;
+import com.okta.examples.apiamhooks.model.hooks.IDTokenPatchResponse;
 import com.okta.examples.apiamhooks.model.hooks.TokenHookRequest;
 import com.okta.examples.apiamhooks.model.hooks.TokenHookResponse;
 import com.okta.examples.apiamhooks.model.hooks.TokenPatchResponse;
 import com.okta.examples.apiamhooks.repository.PersonRepository;
-import com.okta.examples.apiamhooks.service.InlineHookService;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
-import java.util.Map;
 import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("/api/hooks")
 public class HooksController {
 
-    private InlineHookService inlineHookService;
     private PersonRepository personRepository;
 
-    public HooksController(InlineHookService inlineHookService, PersonRepository personRepository) {
-        this.inlineHookService = inlineHookService;
+    public HooksController(PersonRepository personRepository) {
         this.personRepository = personRepository;
     }
 
@@ -51,11 +45,5 @@ public class HooksController {
 
     private List<String> transformBeers(List<Beer> beers) {
         return beers.stream().map(Beer::getName).collect(Collectors.toList());
-    }
-
-    @PostMapping("/create-hook")
-    public @ResponseBody
-    Map<String, Object> createHook(@RequestBody CreateHookRequest createHookRequest) throws Exception {
-        return inlineHookService.createInlineHook(createHookRequest);
     }
 }
